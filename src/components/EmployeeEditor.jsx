21df6f7a880024b1605533b7f821f875a657b37d
Tristan Dincau <tdinca867@west-mec.org>
2024-12-12 16:50:28 -0700

    table

--- src/components/EmployeeEditor.jsx
+++ src/components/EmployeeEditor.jsx
@@ -1,34 +1,14 @@
 export default function EmployeeEditor() {
   return (
     <>
-      <h1>Flight Editor</h1>
+      <h1>Employee Editor</h1>
       <form id="updateForm" class="flight-form">
         <label for="currentId">Flight ID:</label>
         <input type="text" id="currentId" required />
         <br />
         <br />
 
-        <label for="currentOrigin">Current Origin:</label>
-        <input type="text" id="currentOrigin" required />
-        <br />
-        <br />
-
-        <label for="currentDestination">Current Destination:</label>
-        <input type="text" id="currentDestination" required />
-        <br />
-        <br />
-
-        <label for="currentDate">Current Date:</label>
-        <input type="text" id="currentDate" required />
-        <br />
-        <br />
-
-        <label for="currentTime">Current Time:</label>
-        <input type="text" id="currentTime" required />
-        <br />
-        <br />
-
-        <label for="newOrigin">New Origin:</label>
+        <label for="newOrigin">New Name:</label>
         <input type="text" id="newOrigin" required />
         <br />
         <br />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,49 @@
 import { Link } from "react-router-dom";
+import Search from "../components/Search";
 import styles from "../styles/Navbar.module.css";
 export default function Navbar() {
   return (
     <nav className={styles.nav}>
-      <Link to="/">Home</Link>
-      <Link to="/employees">Employees</Link>
+      <div className={`${styles.col} ${styles.brand}`}>
+        <Link className={styles.logoWrapper} to="/">
+          <img
+            className={styles.logo}
+            src="/MECAZON_logo.svg"
+            alt="mecazon logo"
+            height="35px"
+            width="35px"
+          />
+          <p>MECAZON</p>
+        </Link>
+      </div>
+
+      <div className={styles.col}>
+        <Link to="/groceries">
+          <Search></Search>
+        </Link>
+      </div>
+
+      <div className={`${styles.col} ${styles.userButtons}`}>
+        <Link className={styles.loginLink} to="/login">
+          <p>Sign in</p>
+
+          <img
+            src="/account_icon.svg"
+            height="30px"
+            width="30px"
+            alt="user account icon"
+          />
+        </Link>
+
+        <Link className={styles.cartLink} to="/cart">
+          <img
+            src="/cart_icon.svg"
+            alt="shopping cart icon"
+            height="50px"
+            width="50px"
+          />
+        </Link>
+      </div>
     </nav>
   );
 }
