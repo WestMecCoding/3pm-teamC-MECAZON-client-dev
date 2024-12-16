7c095b2bc73cd5682701b7a3c70414b5c2a94f5a
Jaycee Kesterson <jkeste616@west-mec.org>
2024-12-16 16:50:40 -0700

    styles

--- src/components/EmployeeEditor.jsx
+++ src/components/EmployeeEditor.jsx
@@ -1,30 +1,30 @@
 export default function EmployeeEditor() {
-  // return (
-  //   <>
-  //     <h1>Employee Editor</h1>
-  //     <form id="createUser" className="user-form">
-  //       <label htmlFor="firstName">First Name:</label>
-  //       <input type="text" name="firstName" />
-  //       <label htmlFor="lastName">Last Name:</label>
-  //       <input type="text" name="lastName" />
-  //       <label htmlFor="password">Password:</label>
-  //       <input type="text" name="password" />
-  //       <label htmlFor="department">Department:</label>
-  //       <input type="text" name="department" />
-  //       <label htmlFor="employeeType">Employee Type:</label>
-  //       <input type="text" name="employeeType" />
-  //       <label htmlFor="dateHired">Date Hired:</label>
-  //       <input type="text" name="dateHired" />
-  //       <label htmlFor="isAdmin">Admin Status:</label>
-  //       <input type="text" name="isAdmin" />
-  //       <label htmlFor="email">Email:</label>
-  //       <input type="text" name="email" />
-  //       <label htmlFor="phoneNumber">Phone Number:</label>
-  //       <input type="text" name="phoneNumber" />
-  //       <label htmlFor="address">Address:</label>
-  //       <input type="text" name="address" />
+  return (
+    <>
+      <h1>Employee Editor</h1>
+      <form id="createUser" className="user-form">
+        <label htmlFor="firstName">First Name:</label>
+        <input type="text" name="firstName" />
+        <label htmlFor="lastName">Last Name:</label>
+        <input type="text" name="lastName" />
+        <label htmlFor="password">Password:</label>
+        <input type="text" name="password" />
+        <label htmlFor="department">Department:</label>
+        <input type="text" name="department" />
+        <label htmlFor="employeeType">Employee Type:</label>
+        <input type="text" name="employeeType" />
+        <label htmlFor="dateHired">Date Hired:</label>
+        <input type="text" name="dateHired" />
+        <label htmlFor="isAdmin">Admin Status:</label>
+        <input type="text" name="isAdmin" />
+        <label htmlFor="email">Email:</label>
+        <input type="text" name="email" />
+        <label htmlFor="phoneNumber">Phone Number:</label>
+        <input type="text" name="phoneNumber" />
+        <label htmlFor="address">Address:</label>
+        <input type="text" name="address" />
 
-  //     </form>
-  //   </>
-  // );
+      </form>
+    </>
+  );
 }
